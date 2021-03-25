e086a6796cd44cb941e78ff84732f6b92dfc89a0
Dohyun Park <dhlife09@gmail.com>
2021-03-25 13:57:08 +0900

    Update 20210325_1.c

--- 20210325_1.c
+++ 20210325_1.c
@@ -2,8 +2,9 @@ void setup(){
   Serial.begin(9600);
 }
 void loop(){
-  Serial.print("arduino");
+  Serial.print("arduino ");
   Serial.print("world!!");
+  Serial.end();
 }
 
 //arduino 프로그램에서 돋보기 모양을 누르면 시리얼모니터 확인가능
